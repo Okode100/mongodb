8e0a39320b93d0ae8fe1372ec8228ec22f31dd37
Okode100 <alfakings73@gmail.com>
2025-08-14 11:21:52 +0300

    update: app.js create

---
+++ search/app.js
@@ -0,0 +1,18 @@
+ {
+   "name": "plot_title_idx",
+   "collectionName": "movies",
+   "database": "sample_mflix",
+   "definition":{
+     "mappings": {
+       "dynamic": false,
+       "fields": {
+         "plot": {
+           "type": "string"
+         },
+         "title": {
+           "type": "string"
+         }
+       }
+     }
+   }
+ }
---
+++ search/app1.js
@@ -0,0 +1,12 @@
+{
+  "name": "movies_dynamic",
+  "collectionName": "movies",
+  "database": "sample_mflix",
+  "definition":{
+    "searchAnalyzer": "lucene.standard",
+    "analyzer": "lucene.standard",
+    "mappings": {
+      "dynamic": true
+    }
+  }
+}
